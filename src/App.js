f9f4c1bc22e9473f8db6b13fbb83bb0cc6001cba
Aqsa Aqeel <aqsa.aqeel17@example.com>
2023-08-22 16:18:36 +0530

    feat: completed homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-
+import { Homepage } from "./components";
+import "./App.css"
 function App() {
   return (
     <div className="App">
+      <Homepage />
     </div>
   );
 }
--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,17 +1,31 @@
+import "./Homepage.css";
 export const Homepage = () => {
   return (
     <div className="homepage-container">
       <section className="left-container">
-        <img src="images/food-truck.png" alt="food truck" />
-        <h1>
-          Discover the <span>Best</span> Food and Drinks
-        </h1>
-        <p>Naturally made Healthcare Products for the better care & support of your body.</p>
-        <img src="images/explorenow-b-red.png" alt="explore-button"/>
+        <img
+          className="truck-image"
+          src="images/food-truck.png"
+          alt="food truck"
+        />
+        <div className="text-container">
+          <h1 className="main-heading">
+            Discover the <span className="red">Best</span> Food and Drinks
+          </h1>
+          <p className="gray small-text">
+            Naturally made Healthcare Products for the better care & support of
+            your body.
+          </p>
+          <img
+            className="explore-button"
+            src="images/explorenow-b-red.png"
+            alt="explore-button"
+          />
+        </div>
       </section>
       <section className="right-container">
-        <img src="images/pizza.png" alt="pizza" />
-        
+        <img className="pizza-image" src="images/pizza.png" alt="pizza" />
+        <img className="curve-image" src="images/curve.png" alt="curve" />
       </section>
     </div>
   );
