f84f41773aa1c041b88fbd24f3052945e65b3883
Aqsa Aqeel <aqsa.aqeel17@example.com>
2023-08-22 13:52:46 +0530

    fead: added images

---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,18 @@
+export const Homepage = () => {
+  return (
+    <div className="homepage-container">
+      <section className="left-container">
+        <img src="images/food-truck.png" alt="food truck" />
+        <h1>
+          Discover the <span>Best</span> Food and Drinks
+        </h1>
+        <p>Naturally made Healthcare Products for the better care & support of your body.</p>
+        <img src="images/explorenow-b-red.png" alt="explore-button"/>
+      </section>
+      <section className="right-container">
+        <img src="images/pizza.png" alt="pizza" />
+        
+      </section>
+    </div>
+  );
+};
